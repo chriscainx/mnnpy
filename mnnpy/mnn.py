30bcf0e910ee2cdb2285b9e001009f4aa390bb3d
Chris Kang <kbxchrs@gmail.com>
2018-04-26 23:40:02 +0800

    Fixed some docstrings.

--- mnnpy/mnn.py
+++ mnnpy/mnn.py
@@ -14,61 +14,70 @@ def mnn_correct(*datas, var_index=None, var_subset=None, batch_key='batch', inde
                 svd_dim=None, var_adj=True, compute_angle=False, mnn_order=None, svd_mode='rsvd',
                 do_concatenate=True, save_raw=False, n_jobs=None, **kwargs):
     """
-    Apply MNN correct to input data matrices or AnnData objects. Depending on do_concatenate, returns matrices or
-    AnnData objects in the original order containing corrected expression values, or concatenated matrices or AnnData
-    object.
+    Apply MNN correct to input data matrices or AnnData objects. Depending on do_concatenate,
+    returns matrices or AnnData objects in the original order containing corrected expression
+    values, or concatenated matrices or AnnData object.
 
     :param datas: `numpy.ndarray` or class:`anndata.AnnData`
-        Expression matrices or AnnData objects. Matrices should be shaped like n_obs * n_vars (n_cell * n_gene) and have
-        consistent number of columns. AnnData objects should have same number of vars.
+        Expression matrices or AnnData objects. Matrices should be shaped like n_obs * n_vars
+        (n_cell * n_gene) and have consistent number of columns. AnnData objects should have same
+        number of vars.
 
     :param var_index: `list` or `None`, optional (default: None)
-        The index (list of str) of vars (genes). Necessary when using only a subset of vars to perform MNN correction,
-        and should be supplied with var_subset. When datas are AnnData objects, var_index is ignored.
+        The index (list of str) of vars (genes). Necessary when using only a subset of vars to
+        perform MNN correction, and should be supplied with var_subset. When datas are AnnData
+        objects, var_index is ignored.
 
     :param var_subset: `list` or `None`, optional (default: None)
-        The subset of vars (list of str) to be used when performing MNN correction. Typically, a list of highly variable
-        genes (HVGs). When set to None, uses all vars.
+        The subset of vars (list of str) to be used when performing MNN correction. Typically, a
+        list of highly variable genes (HVGs). When set to None, uses all vars.
 
     :param batch_key: `str`, optional (default: 'batch')
-        The batch_key for AnnData.concatenate. Only valid when do_concatenate and supplying AnnData objects.
+        The batch_key for AnnData.concatenate. Only valid when do_concatenate and supplying AnnData
+        objects.
 
     :param index_unique: `str`, optional (default: '-')
-        The index_unique for AnnData.concatenate. Only valid when do_concatenate and supplying AnnData objects.
+        The index_unique for AnnData.concatenate. Only valid when do_concatenate and supplying
+        AnnData objects.
 
     :param batch_categories: `list` or `None`, optional (default: None)
-        The batch_categories for AnnData.concatenate. Only valid when do_concatenate and supplying AnnData objects.
+        The batch_categories for AnnData.concatenate. Only valid when do_concatenate and supplying
+        AnnData objects.
 
     :param k: `int`, optional (default: 20)
         Number of mutual nearest neighbors.
 
     :param sigma: `float`, optional (default: 1)
-        The bandwidth of the Gaussian smoothing kernel used to compute the correction vectors. Default is 1.
+        The bandwidth of the Gaussian smoothing kernel used to compute the correction vectors.
 
     :param cos_norm_in: `bool`, optional (default: True)
-        Whether cosine normalization should be performed on the input data prior to calculating distances between cells.
+        Whether cosine normalization should be performed on the input data prior to calculating
+        distances between cells.
 
     :param cos_norm_out: `bool`, optional (default: True)
-        Whether cosine normalization should be performed prior to computing corrected expression values.
+        Whether cosine normalization should be performed prior to computing corrected expression
+        values.
 
     :param svd_dim: `int` or `None`, optional (default: None)
-        The number of dimensions to use for summarizing biological substructure within each batch. If None, biological
-        components will not be removed from the correction vectors.
+        The number of dimensions to use for summarizing biological substructure within each batch.
+        If set to None, biological components will not be removed from the correction vectors.
 
     :param var_adj: `bool`, optional (default: True)
-        Whether to adjust variance of the correction vectors. Note this step takes most computing time.
+        Whether to adjust variance of the correction vectors. Note this step takes most computing
+        time.
 
     :param compute_angle: `bool`, optional (default: False)
-        Whether to compute the angle between each cell’s correction vector and the biological subspace of the reference
-        batch.
+        Whether to compute the angle between each cell’s correction vector and the biological
+        subspace of the reference batch.
 
     :param mnn_order: `list` or `None`, optional (default: None)
-        The order in which batches are to be corrected. When set to None, datas are corrected sequentially.
+        The order in which batches are to be corrected. When set to None, datas are corrected
+        sequentially.
 
     :param svd_mode: `str`, optional (default: 'rsvd')
-        One of 'svd', 'rsvd', and 'irlb'. 'svd' computes SVD using a non-randomized SVD-via-ID algorithm, while 'rsvd'
-        uses a randomized version. 'irlb' performes truncated SVD by implicitly restarted Lanczos bidiagonalization
-        (forked from https://github.com/airysen/irlbpy).
+        One of 'svd', 'rsvd', and 'irlb'. 'svd' computes SVD using a non-randomized SVD-via-ID
+        algorithm, while 'rsvd' uses a randomized version. 'irlb' performes truncated SVD by
+        implicitly restarted Lanczos bidiagonalization (forked from https://github.com/airysen/irlbpy).
 
     :param do_concatenate: `bool`, optional (default: True)
         Whether to concatenate the corrected matrices or AnnData objects. Default is True.
@@ -84,7 +93,8 @@ def mnn_correct(*datas, var_index=None, var_subset=None, batch_key='batch', inde
 
     :return:
         datas: `numpy.ndarray` or class:`anndata.AnnData`
-            Corrected matrix/matrices or AnnData object/objects, depending on the input type and do_concatenate.
+            Corrected matrix/matrices or AnnData object/objects, depending on the input type and
+            do_concatenate.
 
         mnn_list_: `list`
             A list containing MNN pairing information as DataFrames in each iteration step.
@@ -142,7 +152,8 @@ def mnn_correct(*datas, var_index=None, var_subset=None, batch_key='batch', inde
             raise ValueError('The input matrices have inconsistent number of columns.')
     # ------------------------------------------------------------
     print('Performing cosine normalization...')
-    in_batches, out_batches, var_subset, same_set = transform_input_data(datas, cos_norm_in, cos_norm_out, var_index,
+    in_batches, out_batches, var_subset, same_set = transform_input_data(datas, cos_norm_in,
+                                                                         cos_norm_out, var_index,
                                                                          var_subset, n_jobs)
     if mnn_order is None:
         mnn_order = list(range(n_batch))
@@ -167,8 +178,8 @@ def mnn_correct(*datas, var_index=None, var_subset=None, batch_key='batch', inde
         mnn_ref, mnn_new = find_mutual_nn(data1=ref_batch_in, data2=new_batch_in, k1=k, k2=k,
                                           n_jobs=n_jobs)
         print('  Computing correction vectors...')
-        correction_in = compute_correction(ref_batch_in, new_batch_in, mnn_ref, mnn_new, new_batch_in,
-                                           sigma)
+        correction_in = compute_correction(ref_batch_in, new_batch_in, mnn_ref, mnn_new,
+                                           new_batch_in, sigma)
         if not same_set:
             correction_out = compute_correction(ref_batch_out, new_batch_out, mnn_ref, mnn_new,
                                                 new_batch_in, sigma)
@@ -200,7 +211,8 @@ def mnn_correct(*datas, var_index=None, var_subset=None, batch_key='batch', inde
         # ------------------------
         if var_adj:
             print('  Adjusting variance...')
-            correction_in = adjust_shift_variance(ref_batch_in, new_batch_in, correction_in, sigma, n_jobs)
+            correction_in = adjust_shift_variance(ref_batch_in, new_batch_in, correction_in, sigma,
+                                                  n_jobs)
             if not same_set:
                 correction_out = adjust_shift_variance(ref_batch_out, new_batch_out, correction_out,
                                                        sigma, n_jobs, var_subset)
@@ -215,7 +227,9 @@ def mnn_correct(*datas, var_index=None, var_subset=None, batch_key='batch', inde
             ref_batch_out = np.concatenate((ref_batch_out, new_batch_out))
             res_container.append(new_batch_out)
         mnn_container.append(DataFrame(
-            {'new cell': mnn_new, 'ref cell': mnn_ref, 'original batch': [original_batch[mnn] for mnn in mnn_ref]}))
+            {'new cell': mnn_new,
+             'ref cell': mnn_ref,
+             'original batch': [original_batch[mnn] for mnn in mnn_ref]}))
         original_batch += [target] * new_batch_in.shape[0]
     print('MNN correction complete. Gathering output...')
     reflow_order = [0] * n_batch
